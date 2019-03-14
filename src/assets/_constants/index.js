aba4f4537a5789552ddafb10d8a4d275f90d94ee
Taiwo Adepoju <taiwo.adepoju@VentureGardenGroup.Local>
2019-03-14 12:10:22 +0100

    Added email pattern constant

--- src/assets/_constants/index.js
+++ src/assets/_constants/index.js
@@ -5,3 +5,4 @@ export const ERROR_RESPONSE = 2
 export const BASE_API_URL = 'http://ec2-54-244-201-200.us-west-2.compute.amazonaws.com'
 export const USER_KEY = ''
 export const USER_SESSION_EXPIRED = ''
+export const EMAIL_PATTERN =  new RegExp(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/);
