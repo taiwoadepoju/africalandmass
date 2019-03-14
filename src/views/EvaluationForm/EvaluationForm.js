a9fb327a503c0b58ace1df2a232857679da5c2bf
Taiwo Adepoju <taiwo.adepoju@VentureGardenGroup.Local>
2019-03-14 11:53:28 +0100

    Added functionality to form

--- src/views/EvaluationForm/EvaluationForm.js
+++ src/views/EvaluationForm/EvaluationForm.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { Input, Label, Col, Row, FormGroup } from "reactstrap";
-import { YES, NO } from '../../assets/_constants';
+import { YES, NO, SUCCESS_RESPONSE } from '../../assets/_constants';
 import Datetime from "react-datetime";
 import moment from "moment";
 import Select from "react-select";
-import Dropzone from 'react-dropzone'
+import Dropzone from 'react-dropzone';
+import { evaluatorService } from '../../assets/_services/evaluator.service';
+import notify from '../../assets/_utils/notify';
 
 const selectOptions = [
   { label: "Lagos", value: "Lagos" },
-  { label: "Ikeja", value: "Hey" },
-  { label: "Ikorodu", value: "uu" },
+  { label: "Ikeja", value: "Ikeja" },
+  { label: "Ikorodu", value: "Ikorodu" },
 ]
 
 const fileUploadStyle = {
@@ -24,13 +26,22 @@ class EvaluationForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      loading: false,
+      triggeredSubmit: false,
       existingStructure: "",
-      selectedDate: "",
-      selectedLocation: "",
-      preview: null,
+      existingStructureDetails: "",
+      dateTIme: "",
+      selectedFiles: [],
       firstName: props.location.state && props.location.state.firstName,
       lastName: props.location.state && props.location.state.lastName,
-      email: props.location.state && props.location.state.email
+      email: props.location.state && props.location.state.email,
+      description: "",
+      location: "",
+      size: "",
+      phone: "",
+      titleDeed: "",
+      address: "",
+      uploadedFile: []
     }
   }
 
@@ -41,15 +52,96 @@ class EvaluationForm extends Component {
   }
 
   handleChangeDate = (date) => {
-    return this.setState({ selectedDate: moment(date._d).format("YYYY-MM-DD") })
+    return this.setState({ dateTIme: moment(date._d).format("YYYY-MM-DD") })
   }
 
-  handleDrop = (preview) => {
-    this.setState({ preview })
+  handleChange = (e) => {
+    e.persist()
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleDrop = (files) => {
+    if (this.state.selectedFiles.length === 0) {
+      this.setState({ selectedFiles: files })
+    } else {
+      this.setState({ selectedFiles: this.state.selectedFiles.concat(files) })
+    }
+  }
+
+  handleRemoveFile = (idx) => (e) => {
+    this.setState({
+      selectedFiles: this.state.selectedFiles.filter((s, sidx) => idx !== sidx)
+    });
+  }
+
+  handleUpload = async () => {
+    this.setState({ triggeredSubmit: true })
+    const { firstName, lastName, email, location, size, phone, dateTIme, existingStructure, existingStructureDetails, address, selectedFiles } = this.state
+
+    if(firstName === "" || lastName === "" || email === "" || location === "" || size === "" || phone === "" || dateTIme === "" || existingStructure === "" || (existingStructure === "true" && existingStructureDetails === "") || address === ""){
+      notify.toastError("You must fill all required fields!")
+      return;
+    }
+    if(selectedFiles.length < 1){
+      notify.toastError("You must upload all necessary documents!")
+      return;
+    }
+
+    this.setState({ loading: true })
+
+    let formData = new FormData();
+    var arr = selectedFiles;
+    for (var i = 0; i < arr.length; i++) {
+      formData.append('files', arr[i]);
+    }
+    try {
+      const res = await evaluatorService.uploadEvaluationDocuments(formData);
+      await this.setState({ loading: false, triggeredSubmit: false });
+      if (res.status === SUCCESS_RESPONSE) {
+        await this.setState({ uploadedFile: res.response });
+        this.handleSubmit();
+        notify.toastSuccess("Your Request has been submitted successfully");
+      }
+      else {
+        notify.toastError(`${res.response}`);
+      }
+    }
+    catch (error) {
+      notify.toastError(`${error}`);
+    }
+  }
+
+  handleSubmit = () => {
+    const { firstName, lastName, email, description, location, size, phone, dateTIme, existingStructure, existingStructureDetails, address, uploadedFile } = this.state
+    const payload = {
+      firstName,
+      lastName,
+      email,
+      description,
+      location,
+      size,
+      phone,
+      dateTIme,
+      existingStructure,
+      existingStructureDetails,
+      address,
+      uploadedFile
+    }
+    return evaluatorService.requestForEvaluation(payload)
+      .then(res => {
+        this.setState({ loading: false, triggeredSubmit: false })
+        if (res.status === SUCCESS_RESPONSE) {
+          notify.toastSuccess("Successful")
+        } else {
+          notify.toastError(`${res.response}`)
+        }
+      }).catch(error => { notify.toastError(`${error}`) }
+      )
   }
 
   render() {
-    const { selectedDate, preview, existingStructure, firstName, email, lastName } = this.state
+    const { dateTIme, selectedFiles, existingStructure, firstName, email, lastName, address, description, size, phone, existingStructureDetails, loading, triggeredSubmit, location } = this.state
+
     return (
       <div className="evaluation-form-section">
         <section className="container bg-white">
@@ -59,25 +151,30 @@ class EvaluationForm extends Component {
                 <h5 className="mb-3">Land Evaluation Request Form</h5>
                 <div className="row mb-4">
                   <div className="col-6">
-                    <Input type="text" className="form-control" placeholder="First Name" value={firstName} />
+                    <Input type="text" className="form-control" placeholder="First Name" name="firstName" value={firstName} onChange={this.handleChange} />
+                    {(!!triggeredSubmit && firstName === "")&& <p className="text-danger text-left" >First name is required</p>}
                   </div>
                   <div className="col-6">
-                    <Input type="text" className="form-control" placeholder="Last Name" value={lastName} />
+                    <Input type="text" className="form-control" placeholder="Last Name" name="lastName" value={lastName} onChange={this.handleChange} />
+                    {(!!triggeredSubmit && lastName === "") && <p className="text-danger text-left" >Last name is required</p>}
                   </div>
                 </div>
 
                 <div className="row mb-4">
                   <div className="col-6">
-                    <Input type="text" className="form-control" placeholder="Email Address" value={email} />
+                    <Input type="text" className="form-control" placeholder="Email Address" name="email" value={email} onChange={this.handleChange} />
+                    {(!!triggeredSubmit && email === "") && <p className="text-danger text-left" >Email is required</p>}
                   </div>
                   <div className="col-6">
-                    <Input type="text" className="form-control" placeholder="Phone Number" />
+                    <Input type="text" className="form-control" placeholder="Phone Number" name="phone" value={phone} onChange={this.handleChange} />
+                    {(!!triggeredSubmit && phone === "") && <p className="text-danger text-left" >Phone number is required</p>}
                   </div>
                 </div>
 
                 <div className="row mb-4">
                   <div className="col-12">
-                    <Input type="text" className="form-control" placeholder="Address" />
+                    <Input type="text" className="form-control" placeholder="Address" name="address" value={address} onChange={this.handleChange} />
+                    {(!!triggeredSubmit && address === "") && <p className="text-danger text-left" >Address is required</p>}
                   </div>
                 </div>
 
@@ -88,26 +185,22 @@ class EvaluationForm extends Component {
                       classNamePrefix="react-select"
                       placeholder="Select Land Location"
                       options={selectOptions}
-                      onChange={(e) => this.setState({ selectedLocation: e.value })}
+                      onChange={(e) => this.setState({ location: e.value })}
                     />
+                    {(!!triggeredSubmit && location === "") && <p className="text-danger text-left" >Location is required</p>}
                   </div>
                   <div className="col-6">
-                    <Input type="text" className="form-control" placeholder="Land Size: with Longitude &amp; Latitude (If Available)" />
+                    <Input type="text" name="size" value={size} className="form-control" placeholder="Land Size" onChange={this.handleChange} />
+                    {(!!triggeredSubmit && size === "") && <p className="text-danger text-left" >Land size is required</p>}
                   </div>
                 </div>
 
                 <div className="row">
                   <div className="col-12">
-                    <Input type="textarea" className="form-control" placeholder="Land Description" />
+                    <Input type="textarea" name="description" value={description} className="form-control" placeholder="Land Description" onChange={this.handleChange} />
                   </div>
                 </div>
-
-                <div className="row mb-3 mt-3">
-                  <Label className="pr-3 pl-4">Upload Title Deed of Land</Label>
-                  <div className="col-6">
-                    <Input type="file" placeholder="Choose File" />
-                  </div>
-                </div><hr />
+                <hr />
 
                 <div className="row pt-3 pl-3 mb-3">
                   <FormGroup check className="form-check-radio">
@@ -129,11 +222,13 @@ class EvaluationForm extends Component {
                       </Col>
                     </Row>
                   </FormGroup>
+                  {(!!triggeredSubmit && existingStructure === "") && <p className="text-danger text-left" >You must select an option</p>}
                 </div>
 
-                {existingStructure === YES && <div className="row">
+                {existingStructure === "true" && <div className="row">
                   <div className="col-12">
-                    <Input type="textarea" className="form-control" placeholder="Details of Existing Structure" />
+                    <Input type="textarea" name="existingStructureDetails" value={existingStructureDetails} className="form-control" placeholder="Details of Existing Structure" onChange={this.handleChange} />
+                    {(!!triggeredSubmit && (existingStructure === "true" && existingStructureDetails === "")) && <p className="text-danger text-left" >Details of existing structure is required</p>}
                   </div>
                 </div>}
                 <hr />
@@ -141,30 +236,29 @@ class EvaluationForm extends Component {
                 <div className="row mb-3 justify-content-center">
                   <div className="col-6">
                     <Datetime
-                      //defaultValue={moment().add(-1, 'days')}
-                      date={selectedDate}
-                      inputFormat="YYYY-MM-DD"
+                      //closeOnSelect
+                      date={dateTIme}
+                      inputFormat="YYYY-MM-DD hh:mm:ss"
                       dateFormat="MMM, DD, YYYY"
                       onChange={this.handleChangeDate}
                       inputProps={{ placeholder: "Select Evaluation date" }}
                     />
+                    {(!!triggeredSubmit && dateTIme === "") && <p className="text-danger text-left" >Evaluation date is required</p>}
                   </div>
                 </div>
 
                 <div className="row justify-content-center mt-5">
                   <Dropzone onDrop={this.handleDrop} style={fileUploadStyle}>
                     {({ getRootProps, getInputProps }) => (
-                      <section style={fileUploadStyle}>
-                        <div {...getRootProps()}>
-                          <input {...getInputProps()} />
-                          <p>Drag 'n' drop some files here, or click to select files</p>
+                      <section>
+                        <div>
+                          <input className="d-none" />
+                          <p><strong>Upload all files here including title deed of land, pictures and other relevant documents</strong></p><hr />
+                          <p>Drag and drop files here, or click button below to select files</p><br />
                           <button className="theme_btn btn btn-primary">Browse Files</button>
-                          {preview && <div>
-                            {/* {preview.map((img, index) =>
-                              <img src={img} className="w-100 br1" alt="preview" key={index} />
-                            )} */}
-                            {preview.map((file, index) =>
-                              <p key={index}>{file.name}</p>
+                          {selectedFiles && <div className="mt-3">
+                            {selectedFiles.map((file, index) =>
+                              <p key={index}><i className="fa fa-window-close text-danger px-2 remove-document-icon" onClick={this.handleRemoveFile(index)}></i> {file.name}</p>
                             )}
                           </div>}
                         </div>
@@ -175,9 +269,9 @@ class EvaluationForm extends Component {
 
               </div>
 
-              <div className="row justify-content-center mt-5">
-                <button className="theme_btn btn-sm btn-block btn p-2">Submit</button>
-              </div>
+              
+              <button className="theme_btn btn btn-md mx-auto d-block mt-5" onClick={this.handleUpload}>{loading ? "Sending..." : "Submit"}</button>
+              <br/>
             </div>
           </div>
         </section>
