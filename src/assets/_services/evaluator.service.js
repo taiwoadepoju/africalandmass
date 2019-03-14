bb88f4903d66f5a335bd40aed37ec8a74e5ed9be
Taiwo Adepoju <taiwo.adepoju@VentureGardenGroup.Local>
2019-03-14 17:22:54 +0100

    MAde fixes

--- src/assets/_services/evaluator.service.js
+++ src/assets/_services/evaluator.service.js
@@ -9,7 +9,7 @@ export const evaluatorService = {
 
 //Request for evaluation
 function requestForEvaluation(payload) {
-  return appHelpers.postFormDataRequest(`${BASE_API_URL}/land-evaluation-0.0.1-SNAPSHOT/landevaluation/register`, payload, null)
+  return appHelpers.postRequest(`${BASE_API_URL}/land-evaluation-0.0.1-SNAPSHOT/landevaluation/register`, payload, null)
     .then(res => {
       return appHelpers.formatPromiseResponse(res.data)
     }).catch(
