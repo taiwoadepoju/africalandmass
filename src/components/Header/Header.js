5def174c672484afbbc26b0d81e8dfeecd24e31c
Taiwo Adepoju <taiwo.adepoju@VentureGardenGroup.Local>
2019-03-14 11:54:53 +0100

    changed anchor tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,29 +8,19 @@ class Header extends Component {
       <header className="main_header_area sticky-top shadow">   
         <nav className="navbar navbar-expand-lg"> 
           <h6 className="float-left pt-2">Africa Land Mass</h6>
-          <a className="navbar-brand" href="index.html">
+          <Link className="navbar-brand" to="/">
             {/* <img src="images/logo-2.png" alt />   */}
-            
-          </a> 
+          </Link> 
           {/* Small Divice Menu*/}
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar_supported"> 
             <i className="fa fa-bars" />
           </button>
           <div className="collapse navbar-collapse navbar_supported">
             <ul className="navbar-nav"> 
-              {/* <li className="dropdown">
-                <a className="nav-link dropdown-toggle active" href="#" role="button" data-toggle="dropdown">Home</a>
-                <ul className="dropdown-menu">
-                  <li><a href="index.html" className="active">Home App showcase</a></li>
-                  <li><a href="index-saas.html">Home SaaS</a></li>  
-                  <li><a href="index-agency.html">Home Agency</a></li>  
-                  <li><a href="index-seo.html">Home SEO</a></li>  
-                </ul>
-              </li>  */}
-              <li><a href="servises.html">Home</a></li>   
-              <li><a href="contact.html">About Us</a></li>  
-              <li><a href="contact.html">How it works</a></li>  
-              <li><a href="#" className="theme_btn primary btn-sm">Sign in</a></li>  
+              <li><Link to="/">Home</Link></li>   
+              <li><Link to="/">About Us</Link></li>  
+              <li><Link to="/">How it works</Link></li>  
+              <li><Link to="/" className="theme_btn primary btn-sm">Sign in</Link></li>  
             </ul>   
           </div>
         </nav>   
